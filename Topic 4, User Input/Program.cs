6fd8ffd4a5e83ff9017c549f11144a9df2ff4134
Julian McGuire <339743478@GoTVDSB.ca>
2024-10-15 08:50:21 -0400

    somewhat closer to done

--- Topic 4, User Input/Program.cs
+++ Topic 4, User Input/Program.cs
@@ -1,6 +1,8 @@
 ﻿int age;
+double price;
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Enter Your Age: ");
-Int32.TryParse(Console.ReadLine(), out age);
+Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
 Console.WriteLine("Your age is " + age);
 Console.ReadLine();
+Console.WriteLine();
